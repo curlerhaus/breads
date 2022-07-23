b3f544ba75736f2abafe659c71b79ea115a7417d
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-23 11:42:08 -0500

    sending index and show data

--- controllers/bread_controllers.js
+++ controllers/bread_controllers.js
@@ -2,8 +2,11 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+breads.get('/', (req, res) => {
+    res.render('index', {
+        breads: Bread
+    })
+
 });
 
 module.exports = breads
--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/bread_controllers.js')
 app.use('/breads', breadsController)
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.listen(PORT, function () {
     console.log(`listening on port ${PORT}`);
 });
