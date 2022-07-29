d3d14d88231c474c9f9b030bc161879d13e77881
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-07-28 19:02:12 -0500

    edit and update routes

--- views/show.jsx
+++ views/show.jsx
@@ -7,7 +7,6 @@ function Show ({bread, index}) {
       return (
 <Default>
   <h3>{bread.name}</h3>
-  
   <p>
     and it
     {
@@ -21,8 +20,11 @@ function Show ({bread, index}) {
   <form action={`/breads/${index}?_method=DELETE`} method="POST">
       <input type='submit' value="DELETE"/>
     </form>
+    <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
   <li><a href="/breads">Go home</a>
   </li>
+
 </Default>
       )
   }
