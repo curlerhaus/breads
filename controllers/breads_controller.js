8e7d70f309648ded354ad01cfd6189172164ae3b
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-08-08 19:32:29 -0500

    updates

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { deleteMany } = require('../models/bread.js')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const breadSeedData = require('../models/seed.js')
+const Baker = require('../models/baker.js')
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -17,8 +18,18 @@ breads.get('/', (req, res) => {
 
 
 // NEW
+// breads.get('/new', (req, res) => {
+//   res.render('new')
+// })
+
+//New Steph
 breads.get('/new', (req, res) => {
-  res.render('new')
+  Baker.find()
+    .then(foundBakers => {
+      res.render('new', {
+        bakers: foundBakers
+      })
+    })
 })
 
 // EDIT
@@ -48,37 +59,18 @@ breads.post('/', (req, res) => {
 })
 
 //Create Many
+// breads.get('/data/seed', (req, res) => {
+//   Bread.insertMany([
+    
+//   ])
+//     .then(createdBreads => {
+//       res.redirect('/breads')
+//     })
+// })
+
+//Create Many Steph
 breads.get('/data/seed', (req, res) => {
-  Bread.insertMany([
-    {
-      name: 'Rye',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Rachel'
-    },
-    {
-      name: 'French',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-      baker: 'Chandler'
-
-    },
-    {
-      name: 'Gluten Free',
-      hasGluten: false,
-      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Joey'
-
-    },
-    {
-      name: 'Pumpernickel',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Ross'
-
-    }
-  
-  ])
+  Bread.insertMany(breadSeedData)
     .then(createdBreads => {
       res.redirect('/breads')
     })
